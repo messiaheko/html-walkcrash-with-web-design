b2f149b1142a65ac7d742d8cbdc966c34c5ccbf9
messiaheko <125660682+messiaheko@users.noreply.github.com>
2023-02-17 13:21:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Money</title>
+    <link rel="stylesheet" href="https://.cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integri>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    
+</body>
+</html>
